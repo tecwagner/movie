f38df44552bbeef442cfeec9061c2e87b21858dc
Wagner <wagneroliveirarodrigues@gmail.com>
2021-07-06 19:55:37 -0300

    Ambiente Dev

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Movie React</header>
+    </div>
+  );
+}
+
+export default App;
