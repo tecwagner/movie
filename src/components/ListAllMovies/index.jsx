3ed44c3de7521c21f71ebefaef5902b66b8beade
wagner <wagneroliveirarodrigues@gmail.com>
2023-09-15 14:24:06 -0300

    fix: correção no filtro de pesquisa por nomes de filme

--- src/components/ListAllMovies/index.jsx
+++ src/components/ListAllMovies/index.jsx
@@ -1,166 +1,160 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from "react";
-import { useDebounce } from "use-lodash-debounce";
-import Card from "@material-ui/core/Card";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import PhotoCameraIcon from "@material-ui/icons/PhotoCamera";
-import Typography from "@material-ui/core/Typography";
-import { MoviesServices, MoviesSearchServices } from "../../services/movies";
-import "./list.style.css";
-import { FormataDate } from "../../utils/mask";
-import { useHistory } from "react-router-dom";
-import Button from "@material-ui/core/Button";
+import React, {useEffect, useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import {useDebounce} from 'use-lodash-debounce';
+import Card from '@material-ui/core/Card';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import InputBase from '@material-ui/core/InputBase';
+import {MoviesServices, MoviesSearchServices} from '../../services/movies';
+import {FormataDate} from '../../utils/mask';
+import './list.style.css';
 
 export default function ListAllMovies() {
-  const moviesServices = new MoviesServices();
-  const moviesSearchServices = new MoviesSearchServices();
+	const moviesServices = new MoviesServices();
+	const moviesSearchServices = new MoviesSearchServices();
+	const history = useHistory();
 
-  const [movies, setMovies] = useState([]);
-  const [dates, setDates] = useState({});
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPage] = useState(0);
-  const [totalResuts, settotalResuts] = useState(0);
-  const [searchMovies, setSearchMovies] = useState("");
-  const debouncedValue = useDebounce(searchMovies, 5000);
-  const history = useHistory();
+	const [movies, setMovies] = useState([]);
+	const [dates, setDates] = useState({});
+	const [page, setPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(0);
+	const [total_results, setTotalResults] = useState(0);
+	const [searchMovies, setSearchMovies] = useState('');
+	const debouncedValue = useDebounce(searchMovies, 500);
 
-  const getSearchMovies = async () => {
-    const res = await moviesSearchServices.getSearchMovies(searchMovies, page);
+	// const getResultInfo = () => ({
+	// 	total_results
+	// })
 
-    if (res) {
-      setMovies(res.results);
-    }
-  };
+	const getSearchMovies = async () => {
+		const res = await moviesSearchServices.getSearchMovies(searchMovies, page);
+		if (res) {
+			console.log('list', res);
+			setMovies(res.results);
+			setPage(res.page);
+			setTotalPages(res.total_pages);
+			setTotalResults(res.total_results);
+		}
+	};
 
-  const getListAllMovies = async () => {
-    const res = await moviesServices.getListAllMovies(page);
-    if (res) {
-      setMovies(res.results);
-      setDates(res.dates);
-      setPage(res.page);
-      setTotalPage(res.total_pages);
-      settotalResuts(res.total_results);
-    }
-  };
+	const getListAllMovies = async () => {
+		const res = await moviesServices.getListAllMovies(page);
+		if (res) {
+			setMovies(res.results);
+			setDates(res.dates);
+			setPage(res.page);
+			setTotalPages(res.total_pages);
+			setTotalResults(res.total_results);
+		}
+	};
 
-  useEffect(async () => {
-    if (searchMovies) {
-      getSearchMovies();
-    } else {
-      getListAllMovies();
-    }
-  }, [debouncedValue, page]);
+	useEffect(() => {
+		async function fetchData() {
+			console.log('carregando filter:', searchMovies);
+			if (searchMovies) {
+				await getSearchMovies();
+			} else {
+				await getListAllMovies();
+			}
+		}
 
-  const handleChange = (value) => {
-    setSearchMovies(value);
-  };
+		fetchData();
+	}, [debouncedValue, page]);
 
-  const handleClick = (index) => {
-    if (!(index < 0 && page === 0)) setPage((prev) => prev + index);
-  };
+	const handleChange = (event) => {
+		setSearchMovies(event.target.value);
+	};
 
-  const handleDetails = (movie) => {
-    history.push({ pathname: `/detalhes/${movie.id}`, state: movie });
-  };
+	const handleButtonClick = (increment) => {
+		const nextPage = page + increment;
+		if (nextPage >= 1 && nextPage <= totalPages) {
+			setPage(nextPage);
+		}
+	};
 
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <div>
-        <input
-          type="text"
-          placeholder="Escolha o filme..."
-          onChange={(e) => handleChange(e.target.value)}
-          value={searchMovies}
-        />
-      </div>
-      <main>
-        <Container className={`cardGrid`} maxWidth="lg">
-          <Grid container spacing={2}>
-            {movies.map((movie) => (
-              <Grid item key={movie.id} xs={12} sm={6} md={4}>
-                <div onClick={() => handleDetails(movie)}>
-                  <Card className={`card`}>
-                    {movie.backdrop_path != null ? (
-                      <CardMedia
-                        className={`cardMedia`}
-                        image={
-                          "https://image.tmdb.org/t/p/w500/" +
-                          movie.backdrop_path
-                        }
-                        title={movie.title}
-                      />
-                    ) : (
-                      <CardMedia className={`cardIcon`}>
-                        <PhotoCameraIcon />
-                      </CardMedia>
-                    )}
+	const handleDetails = (movie) => {
+		history.push({pathname: `/detalhes/${movie.id}`, state: movie});
+	};
 
-                    <CardContent className={`cardContent`}>
-                      <Typography className={`cardTitle`}>
-                        {movie.title}
-                      </Typography>
-                      <Typography className={`cardRealese`}>
-                        Lançamento {FormataDate(movie.release_date)}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </div>
-              </Grid>
-            ))}
-          </Grid>
-          <Grid
-            className={`button`}
-            style={{ color: "inherit", textDecoration: "inherit" }}
-          >
-            {page > 1 ? (
-              <Button
-                className="button-anterior"
-                size="small"
-                color="primary"
-                variant="contained"
-                onClick={(e) => handleClick(-1)}
-              >
-                Anterior
-              </Button>
-            ) : (
-              <Button
-                className="button-anterior"
-                size="small"
-                variant="contained"
-                disabled
-              >
-                Anterior
-              </Button>
-            )}
-            {page !== totalPages ? (
-              <Button
-                className="button-proximo"
-                size="small"
-                color="primary"
-                variant="contained"
-                onClick={(e) => handleClick(+1)}
-              >
-                Próximo
-              </Button>
-            ) : (
-              <Button
-                className="button-proximo"
-                size="small"
-                variant="contained"
-                disabled
-              >
-                Próximo
-              </Button>
-            )}
-          </Grid>
-          <div></div>
-        </Container>
-      </main>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<div>
+				<InputBase
+					type="text"
+					placeholder="Escolha o filme..."
+					onChange={handleChange}
+					value={searchMovies}
+				/>
+			</div>
+			<main>
+				<Container className={`cardGrid`} maxWidth="lg">
+					<Grid container spacing={2}>
+						{movies.map((movie) => (
+							<Grid item key={movie.id} xs={12} sm={6} md={4}>
+								<div onClick={() => handleDetails(movie)}>
+									<Card className={`card`}>
+										{movie.backdrop_path != null ? (
+											<CardMedia
+												className={`cardMedia`}
+												image={
+													'https://image.tmdb.org/t/p/w500/' +
+													movie.backdrop_path
+												}
+												title={movie.title}
+											/>
+										) : (
+											<CardMedia className={`cardIcon`}>
+												<PhotoCameraIcon />
+											</CardMedia>
+										)}
+
+										<CardContent className={`cardContent`}>
+											<Typography className={`cardTitle`}>
+												{movie.title}
+											</Typography>
+											<Typography className={`cardRealese`}>
+												Lançamento {FormataDate(movie.release_date)}
+											</Typography>
+										</CardContent>
+									</Card>
+								</div>
+							</Grid>
+						))}
+					</Grid>
+					<Grid
+						className={`button`}
+						style={{color: 'inherit', textDecoration: 'inherit'}}
+					>
+						<Button
+							className="button-anterior"
+							size="small"
+							color="primary"
+							variant="contained"
+							onClick={() => handleButtonClick(-1)}
+							disabled={page <= 1}
+						>
+							Anterior
+						</Button>
+						<Button
+							className="button-proximo"
+							size="small"
+							color="primary"
+							variant="contained"
+							onClick={() => handleButtonClick(1)}
+							disabled={page >= totalPages}
+						>
+							Próximo
+						</Button>
+					</Grid>
+				</Container>
+			</main>
+		</React.Fragment>
+	);
 }
--- src/services/movies.js
+++ src/services/movies.js
@@ -1,29 +1,29 @@
-import axios from "axios";
+import axios from 'axios';
 const URL = `https://api.themoviedb.org/3`;
 const API_KEY = `fe65f8e840e15d06c5c00bf13084da74`;
 
 export class MoviesServices {
-  async getListAllMovies(page) {
-    try {
-      const resp = await axios.get(
-        `${URL}/movie/upcoming?api_key=${API_KEY}&language=pt-BR&page=${page}`
-      );
-      return resp.data;
-    } catch (error) {
-      return error;
-    }
-  }
+	async getListAllMovies(page) {
+		try {
+			const resp = await axios.get(
+				`${URL}/movie/upcoming?api_key=${API_KEY}&language=pt-BR&page=${page}`
+			);
+			return resp.data;
+		} catch (error) {
+			return error;
+		}
+	}
 }
 
 export class MoviesSearchServices {
-  async getSearchMovies(query, page) {
-    try {
-      const resp = await axios.get(
-        `${URL}/search/movie/?api_key=${API_KEY}&language=pt-BR&page=${page}&query=${query}`
-      );
-      return resp.data;
-    } catch (error) {
-      return error;
-    }
-  }
+	async getSearchMovies(query, page) {
+		try {
+			const resp = await axios.get(
+				`${URL}/search/movie?query=${query}&page=${page}&api_key=${API_KEY}&language=pt-BR`
+			);
+			return resp.data;
+		} catch (error) {
+			return error;
+		}
+	}
 }
